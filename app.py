f7904b0300e49bfb360aefacf39a17743eb3f040
Justin Schultz <SchultzJustin@ymail.com>
2025-07-22 23:40:51 +0000

    Create basic file/content display functionality

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
+import os
 from flask import (
                     Flask,
                     render_template,
                     redirect,
                     request,
+                    send_from_directory,
                     url_for,
                     )
 
@@ -10,7 +12,22 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('layout.html')
+    return redirect(url_for('get_files'))
+
+@app.route('/files')
+def get_files():
+    root = os.path.abspath(os.path.dirname(__file__))
+    data_dir = os.path.join(root, 'cms', 'data')
+    files = [os.path.basename(path) for path in os.listdir(data_dir)]
+
+    return render_template('files.html', files=files)
+
+@app.route('/files/<filename>')
+def display_file(filename):
+    root = os.path.abspath(os.path.dirname(__file__))
+    data_dir = os.path.join(root, 'cms', 'data')
+
+    return send_from_directory(data_dir, filename)
 
 if __name__ == "__main__":
     app.run(debug=True, port=8080)
